b018c5a3d3b16465073ecc2cf6e241928030f870
Juan Agudelo <verojuan9007@gmail.com>
2025-08-28 20:58:47 -0500

    Testing some styles on chatList

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,13 @@ const App = () => {
         logOut();
       }
     });
-  
+
     return () => unSub();
   }, [userInfo, logOut]);
-  
-   
 
   return (
-    <div className="w-[90vw] h-[90vh] bg-black/50 backdrop-blur-md rounded-md">
+    <div className="w-[90vw] h-[90vh] bg-black/50 backdrop-blur-md rounded-md p-2">
       {currentUser ? <Home /> : <Login />}
-     
     </div>
   );
 };
--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -10,14 +10,15 @@ const ChatList = () => {
         <div className="relative" id="search">
           <input
             type="search"
-            className="outline-none rounded-md border-1 pl-6"
+            placeholder="Search"
+            className="outline-none rounded-md pl-6 bg-[#c2c2c2]/40"
           />
-          <span className="absolute top-1.5 left-1.5">
+          <span className="absolute top-1 left-1">
             <CiSearch />
           </span>
         </div>
         <div
-          className="flex items-center gap-2 border-b-1  border-white p-1 "
+          className="flex items-center gap-2   bg-white/30 rounded-md p-1 transition-all duration-300 hover:bg-white/70"
           id="profile-info"
         >
           <img className="h-8 w-8 rounded-full" src="./avatar.png" alt="" />
@@ -32,7 +33,7 @@ const ChatList = () => {
           </div>
         </div>
         <div
-          className="flex items-center gap-2 border-b-1  border-white p-1 "
+          className="flex items-center gap-2   bg-white/30 rounded-md p-1 "
           id="profile-info"
         >
           <img className="h-8 w-8 rounded-full" src="./avatar.png" alt="" />
@@ -47,7 +48,7 @@ const ChatList = () => {
           </div>
         </div>
         <div
-          className="flex items-center gap-2 border-b-1  border-white p-1 "
+          className="flex items-center gap-2   bg-white/30 rounded-md p-1 "
           id="profile-info"
         >
           <img className="h-8 w-8 rounded-full" src="./avatar.png" alt="" />
@@ -62,7 +63,7 @@ const ChatList = () => {
           </div>
         </div>
         <div
-          className="flex items-center gap-2 border-b-1  border-white p-1 "
+          className="flex items-center gap-2   bg-white/30 rounded-md p-1 "
           id="profile-info"
         >
           <img className="h-8 w-8 rounded-full" src="./avatar.png" alt="" />
