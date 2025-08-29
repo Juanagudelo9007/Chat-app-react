72d4d130ecdf6c416abbdaf9652218f5205c6071
Juan Agudelo <verojuan9007@gmail.com>
2025-08-28 21:57:58 -0500

    working on middle screen desing (main scree and input text) just design not logic yet..

--- src/components/ChatMessages.jsx
+++ src/components/ChatMessages.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const ChatMessages = () => {
   return (
-    <div className='flex flex-2'>ChatMessages</div>
-  )
-}
+    <div className="flex flex-2 p-2">
+      <div id="top" className="w-full flex flex-col p-2">
+        <div id="profile" className="flex justify-start items-center gap-4">
+          <img src="./avatar.png" alt="" className="h-14 w-14 rounded-xl" />
+          <div id="name-lastOnline" className="flex flex-col">
+            <h1>Gordito</h1>
+            <i className="text-[8px]">Last time Online</i>
+          </div>
+        </div>
+      </div>
+      <div id="mid-messages"></div>
+    </div>
+  );
+};
 
-export default ChatMessages
+export default ChatMessages;
--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import useAuthStore from '../store/userStore';
+import React from "react";
+import useAuthStore from "../store/userStore";
 
 const ProfileDetails = () => {
-        const { logOut } = useAuthStore();
+  const { logOut } = useAuthStore();
   return (
     <div className="flex flex-1">
       ProfileDetails
       <button onClick={logOut}>Log Out</button>
     </div>
   );
-}
+};
 
-export default ProfileDetails
+export default ProfileDetails;
